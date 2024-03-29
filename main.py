b8cebe38e73deb35166419b4d290715462c82e24
antonstrobe <54191567+antonstrobe@users.noreply.github.com>
2024-03-29 16:53:30 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -33,14 +33,29 @@ while ret:
     for license_plate in license_plates.boxes.data.tolist():
         x1, y1, x2, y2, score, class_id = license_plate
         plate_crop = frame[int(y1):int(y2), int(x1):int(x2)]
+        plate_resized = cv2.resize(plate_crop, None, fx=3, fy=3, interpolation=cv2.INTER_LINEAR)
+        border_size = 20
+        plate_with_border = cv2.copyMakeBorder(plate_resized, border_size, border_size, border_size, border_size, cv2.BORDER_CONSTANT, value=[255, 255, 255])
+        start_x = int(x1)
+        start_y = int(y1) - plate_with_border.shape[0] - 150
+        if start_y < 0:
+            start_y = 0
+        end_y = start_y + plate_with_border.shape[0]
+        end_x = start_x + plate_with_border.shape[1]
+        if end_y <= frame.shape[0] and end_x <= frame.shape[1]:
+            frame[start_y:end_y, start_x:end_x] = plate_with_border
         plate_gray = cv2.cvtColor(plate_crop, cv2.COLOR_BGR2GRAY)
         plate_gray = cv2.GaussianBlur(plate_gray, (5, 5), 0)
         plate_binary = cv2.threshold(plate_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
         text = pytesseract.image_to_string(plate_binary, lang='eng', config='--psm 7')
-        cv2.putText(frame, text, (int(x1), int(y2)+20), cv2.FONT_HERSHEY_SIMPLEX, 2.6, (255,0,0), 2)
+        text_size = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 1, 3)[0]
+        text_x = start_x + (plate_with_border.shape[1] - text_size[0]) // 2
+        text_y = start_y - 30
+        cv2.rectangle(frame, (text_x - 10, text_y + 10), (text_x + text_size[0] + 10, text_y - text_size[1] - 10), (255, 255, 255), cv2.FILLED)
+        cv2.putText(frame, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 3)
         cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
 
-    cv2.imshow('Processed Frame', frame)    
+    cv2.imshow('Processed Frame', frame)
     cv2.resizeWindow('Processed Frame', 1080, 720)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
